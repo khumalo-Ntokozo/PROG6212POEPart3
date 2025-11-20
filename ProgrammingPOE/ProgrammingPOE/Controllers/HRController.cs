2266f37c01f064f6fd80f1eb35dc136fcd594678
ST10447179 <ST10447179@imconnect.edu.za>
2025-11-20 13:57:17 +0200

    optimize HR controller with async operations

--- ProgrammingPOE/ProgrammingPOE/Controllers/HRController.cs
+++ ProgrammingPOE/ProgrammingPOE/Controllers/HRController.cs
@@ -33,7 +33,7 @@ namespace ProgrammingPOE.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateUser(string FullName, string Email, string Role, decimal HourlyRate, string Password, string ConfirmPassword)
         {
-            // Create a new model for the view in ca
+            
             var model = new ApplicationUser
             {
                 FullName = FullName,
